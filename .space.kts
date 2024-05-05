8a8892994f250f61397db73aebc01fa48db0066a
admin admin <admin@example.com>
2024-05-05 17:43:53 +0000

    gradle job test; space api test

--- .space.kts
+++ .space.kts
@@ -1,4 +1,4 @@
-job("Hello World!") {
+job("Easy job") {
     container(displayName = "Say Hello", image = "hello-world")
 }
 
@@ -6,7 +6,9 @@ job("Сборка с уведомлением") {
     container(displayName = "Собрать и уведомить об ошибке", image = "gradle:7.1-jre11") {
         kotlinScript { api ->
             try {
-                api.gradle("build")
+                api.gradle("build") {
+        			disableSpaceInitScript = true
+    			}
             } catch (ex: Exception) {
                 val channel = ChannelIdentifier.Channel(ChatChannel.FromName("CI-channel"))
                 val content = ChatMessage.Text("Ошибка сборки проекта")
@@ -23,5 +25,14 @@ job("Сборка Gradle") {
         		disableSpaceInitScript = true
     		}
 		}
+    }
+}
+
+job("Print total members count") {
+    container(image = "amazoncorretto:17-alpine") {
+        kotlinScript { api ->
+            val totalMembers = api.space().teamDirectory.stats.getAllStats().totalMembers
+            println("Total members count: $totalMembers")
+        }
     }
 }
