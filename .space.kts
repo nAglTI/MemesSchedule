2bbc8a1910b69b5cee513a3d801cf99e0ad7ed11
admin admin <admin@example.com>
2024-05-05 14:47:40 +0000

    space api test #1

--- .space.kts
+++ .space.kts
@@ -1,9 +1,17 @@
-/**
-* JetBrains Space Automation
-* This Kotlin script file lets you automate build activities
-* For more info, see https://www.jetbrains.com/help/space/automation.html
-*/
-
 job("Hello World!") {
     container(displayName = "Say Hello", image = "hello-world")
+}
+
+job("Build and publish") {
+    container(displayName = "Build and notify", image = "gradle:7.1-jre11") {
+        kotlinScript { api ->
+            try {
+                api.gradle("build")
+            } catch (ex: Exception) {
+                val channel = ChannelIdentifier.Channel(ChatChannel.FromName("CI-channel"))
+                val content = ChatMessage.Text("Build failed")
+                api.space().chats.messages.sendMessage(channel = channel, content = content)
+            }
+        }
+    }
 }
