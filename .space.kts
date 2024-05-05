6588cf31bcc7f8cc2998dc0ae0c3147a0d49c315
admin admin <admin@example.com>
2024-05-05 14:49:24 +0000

    space api test #2

--- .space.kts
+++ .space.kts
@@ -2,14 +2,14 @@ job("Hello World!") {
     container(displayName = "Say Hello", image = "hello-world")
 }
 
-job("Build and publish") {
-    container(displayName = "Build and notify", image = "gradle:7.1-jre11") {
+job("Сборка с уведомлением") {
+    container(displayName = "Собрать и уведомить об ошибке", image = "gradle:7.1-jre11") {
         kotlinScript { api ->
             try {
                 api.gradle("build")
             } catch (ex: Exception) {
                 val channel = ChannelIdentifier.Channel(ChatChannel.FromName("CI-channel"))
-                val content = ChatMessage.Text("Build failed")
+                val content = ChatMessage.Text("Ошибка сборки проекта")
                 api.space().chats.messages.sendMessage(channel = channel, content = content)
             }
         }
